d81a556bbafd82e6883a378b02e9c0ef02148823
Sardor <sardorsoftwaredeveloper@gmail.com>
2022-02-21 00:20:26 +0100

    initial commit

---
+++ articles/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Article, Comment
+
+# Create class
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInLine]
+
+# Register your models here.
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
