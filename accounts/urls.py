34c368507f1e85f6e78b78ef517089a933c02be3
Sardor <sardorsoftwaredeveloper@gmail.com>
2022-02-25 11:03:32 +0100

    account page edited

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SignUpView, UserEditForm, ShowProfilePageView, EditProfilePageView, CreateProfilePageView
+from .views import SignUpView, UserEditForm, ShowProfilePageView, EditProfilePageView, CreateProfilePageView, AccountShowView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('edit_profile/', UserEditForm.as_view(), name='edit_profile'),
+    path('<int:pk>/account_page/', AccountShowView.as_view(), name='account_page'),
+    path('edit_account/', UserEditForm.as_view(), name='edit_account'),
     path('<int:pk>/profile', ShowProfilePageView.as_view(), name='user_profile'),
     path('<int:pk>/edit_profile_page', EditProfilePageView.as_view(), name='edit_profile_page'),
     path('create_profile_page', CreateProfilePageView.as_view(), name='create_profile_page'),
