9d61d2f0d4f03ad0a8ebe42dd96b055510ebcbd0
Sardor <sardorsoftwaredeveloper@gmail.com>
2022-12-29 16:40:26 +0100

    change design of the transfer_detail page

--- pages/forms.py
+++ pages/forms.py
@@ -5,14 +5,13 @@ class ContactUsForm(forms.ModelForm):
     #salutation = forms.CharField(widget=forms.Select(attrs={'class': "form-control my-2"}))
     firstname = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Firstname', 'class': "form-control my-2"}))
     lastname = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Lastname', 'class': "form-control my-2"}))
-    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': "form-control my-2"}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': "my-2 form-control new-Email"}))
     phone_number = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Phone number', 'class': "form-control my-2"}))
     address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Address', 'class': "form-control my-2"}))
     zip_code = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Zip code', 'class': "form-control my-2"}))
     subject = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'subject', 'class': "form-control my-2"}))
-    #content = forms.CharField(widget=forms.Textarea(attrs={'class': "form-control my-2"}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': "form-control my-2"}))
 
     class Meta:
         model = ContactUs
-        fields = ('salutation', 'firstname', 'lastname', 'email', 'phone_number', 'address', 'zip_code', 'subject', 'content',)
-
+        fields = ['salutation', 'firstname', 'lastname', 'email', 'phone_number', 'address', 'zip_code', 'subject', 'content',]
